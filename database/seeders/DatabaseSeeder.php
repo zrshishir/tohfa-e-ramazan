b2844afd17ac5ed64268e690c7556f915ad1a305
zrshishir <zrshishir@gmail.com>
2023-06-18 14:58:53 +0600

    simple update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-      
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
