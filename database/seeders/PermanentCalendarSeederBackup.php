4be0a5e4a2053eee2887af04755c60d432093b52
xpeedstudio <xpeedstudio@gmail.com>
2024-01-16 11:52:11 +0600

    permanent-calendar structuring

--- database/seeders/PermanentCalendarSeederBackup.php
+++ database/seeders/PermanentCalendarSeederBackup.php
@@ -16,6 +16,29 @@ class PermanentCalendarSeeder extends Seeder
         $sample = [
         'sehri' => json_encode(['text_en' => 'Sehri', 'text_bn' => 'সেহরী', 'text_ar' => 'سحري', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']),
         'fazr' => json_encode(['text_en' => 'Fazr', 'text_bn' => 'ফজর', 'text_ar' => 'فجر', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']),
+        'johr' => json_encode(['text_en' => 'Johr', 'text_bn' => 'জোহর', 'text_ar' => 'جوهر', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']),
+        'asr' => json_encode(['text_en' => 'Asr', 'text_bn' => 'আসর', 'text_ar' => 'عصر', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']),
+        'magrib' => json_encode(['text_en' => 'Magrib', 'text_bn' => 'মাগরিব', 'text_ar' => 'مغرب', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']),
+        'esha' => json_encode(['text_en' => 'Esha', 'text_bn' => 'ঈশা', 'text_ar' => 'عشاء', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']),
+        'jummah' => json_encode(['text_en' => 'Jummah', 'text_bn' => 'জুম্মা', 'text_ar' => 'جمعة', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']),
+        'tahazzud' => json_encode(['text_en' => 'Tahazzud', 'text_bn' => 'তাহাজ্জুদ', 'text_ar' => 'تهجد', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']),
+        'ishraq' => json_encode(['text_en' => 'Ishraq', 'text_bn' => 'ইশরাক', 'text_ar' => 'إشراق', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']),
+        'forbidden' => json_encode(['text_en' => 'Forbidden Time', 'text_bn' => 'নিষিদ্ধ সময়', 'text_ar' => 'وقت محظور', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']),
+        ];
+        // make the following array according to the $sample
+        $permanentCalendars = [
+            [
+                'month_id' => 1,
+                'day' => '01',
+                'sehri' => json_encode(['text_en' => 'Sehri', 'text_bn' => 'সেহরী', 'text_ar' => 'سحري', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']),
+                'fazr' => json_encode(['text_en' => 'Fazr', 'text_bn' => 'ফজর', 'text_ar' => 'فجر', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']),
+                'sunrise' => json_encode(['text_en' => 'Sunrise', 'text_bn' => 'সূর্যোদয়', 'text_ar' => 'شروق الشمس', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']),
+                'johr' => json_encode(['text_en' => 'Johr', 'text_bn' => 'জোহর', 'text_ar' => 'جوهر', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']),
+                'asr' => json_encode(['text_en' => 'Asr', 'text_bn' => 'আসর', 'text_ar' => 'عصر', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']),
+                'magrib' => json_encode(['text_en' => 'Magrib', 'text_bn' => 'মাগরিব', 'text_ar' => 'مغرب', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']),
+                'esha' => json_encode(['text_en' => 'Esha', 'text_bn' => 'ঈশা', 'text_ar' => 'عشاء', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']),
+                'jummah' => json_encode(['text_en' => 'Jummah', 'text_bn' => 'জুম্মা', 'text_ar' => 'جمعة', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']), 'tahazzud' => json_encode(['text_en' => 'Tahazzud', 'text_bn' => 'তাহাজ্জুদ', 'text_ar' => 'تهجد', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']), 'ishraq' => json_encode(['text_en' => 'Ishraq', 'text_bn' => 'ইশরাক', 'text_ar' => 'إشراق', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']), 'forbidden' => json_encode(['text_en' => 'Forbidden Time', 'text_bn' => 'নিষিদ্ধ সময়', 'text_ar' => 'وقت محظور', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM'])
+            ]
         ];
         $permanentCalendars = [
             ['month_id' => 1, 'day' => '01', 'sehri' => json_encode(['text_en' => 'Sehri', 'text_bn' => 'সেহরী', 'text_ar' => 'سحري', 'start_time' => '05:16 AM', 'end_time' => '05:16 AM']), 'fazr_time' => '5:22 AM', 'sunrise_time' => '06:41 PM', 'ishraq_time' => '', 'johr_time' => '12:06 PM', 'asr_time' => '03:46 PM', 'magrib_and_iftar_time' => '05:27 PM', 'esha_time' => '06:45 PM'],
